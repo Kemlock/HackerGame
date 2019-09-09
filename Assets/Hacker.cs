bb0655db05289099445a95edd8deaa3ed19b1c30
u <chris.davey@steelcurve.com>
2019-09-09 21:04:59 +0100

    solo challenge

--- Assets/Hacker.cs
+++ Assets/Hacker.cs
@@ -5,23 +5,84 @@ using UnityEngine;
 
 public class Hacker : MonoBehaviour
 {
+    //Game configuration
+    string[] level1Passwords = { "Bernard", "Fuze", "Tea" };
+    string[] level2Passwords = { "Developer", "Pokemon", "Coffee" };
+
+    // Game state
+    int level;
+    string password;
+    enum Screen {Menu, Password, Win }
+    Screen currentScreen = Screen.Menu;
+
     // Start is called before the first frame update
     void Start()
     {
-        string greeting = "Hello Chemlock....";
 
-        ShowMainMenu(greeting);
+        ShowMainMenu();
     }
 
     void OnUserInput(string input)
     {
-        print(input);
+        if (input.ToLower() == "menu")
+        {
+            ShowMainMenu();
+        }
+        else if (currentScreen == Screen.Menu)
+        {
+            RunMainMenu(input);
+        }
+        else if (currentScreen == Screen.Password)
+        {
+            CheckPassword(input);
+        }
+    }
+    
+    void RunMainMenu(string input)
+    {
+        bool isValidLevelNumber = (input == "1" || input == "2");
+        
+        if (isValidLevelNumber)
+        {
+            level = int.Parse(input);
+            StartGame();
+        }
+        else
+        {
+            Terminal.WriteLine("I don't understand....");
+        }
     }
 
-    void ShowMainMenu(string greeting)
+    void StartGame()
     {
+        currentScreen = Screen.Password;
+        CreatePassword();
+
         Terminal.ClearScreen();
-        Terminal.WriteLine(greeting);
+        Terminal.WriteLine(">Enter Password:");
+
+    }
+
+    private void CreatePassword()
+    {
+        switch (level)
+        {
+            case 1:
+                password = level1Passwords[UnityEngine.Random.Range(0, level1Passwords.Length)];
+                break;
+            case 2:
+                password = level2Passwords[UnityEngine.Random.Range(0, level2Passwords.Length)];
+                break;
+            default:
+                Debug.LogError("Invalid level number");
+                break;
+        }
+    }
+
+    void ShowMainMenu()
+    {
+        currentScreen = Screen.Menu;
+        Terminal.ClearScreen();
         Terminal.WriteLine(">Bernard O/S v6.22 Active " + DateTime.Today.ToString());
         Terminal.WriteLine(">");
         Terminal.WriteLine(">Press 1 to hack Beau");
@@ -30,4 +91,18 @@ public class Hacker : MonoBehaviour
         Terminal.WriteLine(">");
         Terminal.WriteLine(">Enter your selection:");
     }
+
+   void CheckPassword(string input)
+    {
+        if(input == password)
+        {
+            currentScreen = Screen.Win;
+            Terminal.WriteLine(">LeveL:"+level+" Access Granted");
+        }
+        else
+        {
+            Terminal.WriteLine(">Access Denied");
+        }
+    }
+
 }
